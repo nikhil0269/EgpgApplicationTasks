3978244da74c79aeb0c86f240f335ba2f0f05ccb
nikhil0269 <nikhil0269@gmail.com>
2018-02-25 07:42:54 +0530

    Commenting TowerofHanoi.sh

--- TowerofHanoi.sh
+++ TowerofHanoi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #-----------------------------------------------------------------
 #				 Tower Of Hanoi
+			#	==================
+			#Here I am naming source tower as 1, temprary 
+			#tower as 2 and destnation tower as 3.
+			#and also the name of disks as 1 2 3 ... n from 
+			# top to bottom
 #-----------------------------------------------------------------
 #---------------------------------------------
 #NAME: Nikhil Kumar Mishra
@@ -14,27 +19,27 @@ TowerofHanoi(){
 		local dest=$4
 
 		if [ $n == 1 ];then		#Base case
-			echo "move $n disk from $ini to $dest"
+			echo "move disk $n from tower $ini to tower $dest"
 				return 
 				fi
 				local c=$((n-1))
 
 				TowerofHanoi $c $ini $dest $t                 #Recursive call
-				echo "move $n disk from $ini to $dest"
+				echo "move disk $n from tower $ini to tower $dest"
 				c=$((n-1))
 				TowerofHanoi $c $t $ini $dest                 #Recursive call
 				return 
 }
 
 
-initial=1    		#First piller(initial)
+sour=1    		#First piller(initial)
 temp=2       		#temprary piller
 dest=3	     		#Destination piller
 
 #Reading no. of disks on the tower
 read -p "Enter no. of disks" n
 echo "The order is :"
-TowerofHanoi $n $initial $temp $dest
+TowerofHanoi $n $sour $temp $dest
 
 #-------------------------------------------------------------
 # 		 End of Tower Of Hanoi
